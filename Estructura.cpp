08e8ca45795aa1bfdcd86b411ab0c79033d3a43e
Lizbeth54321 <lm4932830@gmail.com>
2019-08-30 13:48:39 -0500

    La practica nos ayudo a practicar como utilizar los punteros y ha realizar unna o mas estructuras para que el apuntador lo puedad identificar

--- Estructura.cpp
+++ Estructura.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
- struct personas {
- 	char nombres[20];
- 	int edad;
- } persona1,persona2;
+ struct musicas {
+ 	char cancion[20];
+ 	char artista [20];
+ };
+ struct album{
+ 	char genero[20];
+ 	float visitas;
+ 	struct musicas music;
+ };
  int main(int argc,char**argv){
-    cout<<"nombre:";
-    cin.getline(persona2.nombres,20,'\n');
-    cout<<"edad:";
-    cin>>persona2.edad;
-    cout<<persona2.nombres;
-    cout<<persona2.edad;
-    system("pause");
-    return 0;
+ 	struct album a1[5];
+ 	struct album *apuntador;
+ 	
+ 	apuntador=a1;
+ 	for (int i=0;i<2;i++){
+ 		cout<<"genero:";
+ 		cin.getline(a1[i].genero,20,'\n');
+ 		cout<<"visitas:";
+ 		cin>>a1[i].visitas;
+ 		cout<<"cancion:";
+ 		fflush(stdin);
+ 		cin.getline(a1[i].music.cancion,20,'\n');
+ 		
+ 		
+	 }
+	 cout<<endl<<"album guardado:"<<a1[1].genero;
+	 cout<<endl<<"visita guardada: "<<a1[1].visitas;
+	 cout<<endl<<"cancion guardada: "<<a1[1].music.cancion;
+	 
+	 getch();
+	 return 0;
 
  }
  
